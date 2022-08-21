7411a21802b09b3c4b431f04944272435aae5549
Matheus <tksheisters@gmail.com>
2022-08-21 11:43:15 -0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'appgeo';
+
+  map_url = "";
+  latitude = 0;
+  longitude = 0;
+
+  teste= 0;
+  latitude2 = 0;
+  longitude2 = 0;
+
+  ngOnInit() {
+    this.verifica_local();
+  }
+
+  verifica_local() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          this.latitude = pos.coords.latitude;
+          this.longitude = pos.coords.longitude;
+          this.map_url = 'https://www.google.com/maps/search/?api=1&query=' + this.latitude + ',' + this.longitude;
+        },
+        () => {
+          alert("Sem localização!");
+        },
+        { timeout: 15000 }
+      );
+      navigator.geolocation.watchPosition(
+        (pos) => {
+          this.latitude2 = pos.coords.latitude;
+          this.longitude2 = pos.coords.longitude;
+        },
+        () => {
+          alert("Sem rastreamento!")
+        },
+        { timeout: 15000 }
+      );
+    }
+    else {
+      alert("Sem permissão")
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
